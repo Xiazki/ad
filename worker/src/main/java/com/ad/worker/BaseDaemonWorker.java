44e915f39211c38968f793d7ff3832571951895a
xiaxiang <xiaxiang@mixiong.tv>
2017-05-11 13:55:20 +0800

    init commit

---
+++ worker/src/main/java/com/ad/worker/BaseDaemonWorker.java
@@ -0,0 +1,13 @@
+package com.ad.worker;
+
+/**
+ * Created by xiang on 2017/5/11.
+ */
+public abstract class BaseDaemonWorker implements Runnable{
+
+
+    protected void startExecute(){
+
+    }
+
+}
